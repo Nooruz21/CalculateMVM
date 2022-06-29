4d08b011df6d0d18e26d6168db0038ea6012c622
Nooruz21 <awbawb2129@gmail.com>
2022-06-29 23:49:05 +0600

    loveCalculateMVM

--- app/src/main/java/com/example/lovecalculate/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/lovecalculate/fragment/HomeFragment.kt
@@ -10,14 +10,11 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import com.example.lovecalculate.App
 import com.example.lovecalculate.R
+
 import com.example.lovecalculate.databinding.FragmentHomeBinding
-import com.example.lovecalculate.network.model.LoveModel
+
 import com.example.lovecalculate.viewmodel.LoveViewModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 
 class HomeFragment : Fragment() {
@@ -44,32 +41,21 @@ class HomeFragment : Fragment() {
             okBtn.setOnClickListener {
                 val firstName = firstEd.text.toString()
                 val secondName = secondEd.text.toString()
-                App.loveApi.getPercentage(firstName, secondName).enqueue(object :
-                    Callback<LoveModel> {
-                    override fun onResponse(call: Call<LoveModel>, response: Response<LoveModel>) {
-                        if (response.isSuccessful) {
-                            Log.e("ololo", "onResponse:${response.body()?.result}")
-                            val bundle = Bundle()
-
-                            bundle.putString("firstName", response.body()?.firstName)
-                            bundle.putString("secondName", response.body()?.secondName)
-                            bundle.putString("percentage", response.body()?.percentage)
-                            bundle.putString("result", response.body()?.result)
-
-                            navController.navigate(R.id.secondFragment, bundle)
-                        }
-                    }
-
-                    override fun onFailure(call: Call<LoveModel>, t: Throwable) {
-                        Log.e("ololo", "onFailure:${t.message}")
-
                 viewModel.getLiveLoveModel(firstName, secondName)
                     .observe(viewLifecycleOwner, Observer {
                         Log.e("ololo", "initClickers:$it")
+                        val bundle = Bundle()
+
+                        bundle.putString("firstName", firstName)
+                        bundle.putString("secondName", secondName)
+                        bundle.putString("percentage","" )
+                        bundle.putString("result", "")
+
+                        navController.navigate(R.id.secondFragment, bundle)
                     })
             }
-        })
-    }}}}
+        }
+    }}
 
 
 
